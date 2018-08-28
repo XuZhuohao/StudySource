83cd0ec6b713a61d51499fbaa92d1ab19514fbf4
XuZhuohao <786725551@qq.com>
2018-08-28 11:42:44 +0800

    add modules which name is cloud_feign and cloud_hystrix

---
+++ SpringCloud/cloud_feign/src/main/java/com/yui/study/cloud/feign/CloudFeignApplication.java
@@ -0,0 +1,16 @@
+package com.yui.study.cloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class CloudFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFeignApplication.class, args);
+    }
+}

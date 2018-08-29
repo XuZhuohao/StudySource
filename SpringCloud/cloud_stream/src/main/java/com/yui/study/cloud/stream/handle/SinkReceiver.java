cb93d47565ef4eb51df9503d4670729aa13b3741
XuZhuohao <786725551@qq.com>
2018-08-29 16:30:36 +0800

    add source of security

---
+++ SpringCloud/cloud_stream/src/main/java/com/yui/study/cloud/stream/handle/SinkReceiver.java
@@ -0,0 +1,15 @@
+/*
+package com.yui.study.cloud.stream.handle;
+
+*/
+/**
+ * AMQ
+ *
+ * @author XuZhuohao
+ * @date 2018/8/28
+ *//*
+
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+}
+*/
---
+++ security/src/main/java/com/yui/study/security/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.yui.study.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Hello 控制器
+ *
+ * @author XuZhuohao
+ * @date 2018/8/29
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = {"/","/welcome**"},method = RequestMethod.GET)
+    public ModelAndView welcome(){
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+
+    }
+}

59e6d336cc1fa40377e3270e4fbbf33c22c089d5
XuZhuohao <786725551@qq.com>
2019-04-03 14:46:09 +0800

    不同包的 demo

---
+++ zookeeper/demo/src/main/java/cn/com/yui/study/zookeeper/demo/BeanConfig.java
@@ -0,0 +1,14 @@
+package cn.com.yui.study.zookeeper.demo;
+
+/**
+ * @author XuZhuohao
+ * @date 2019/3/27
+ */
+/*@Configuration
+public class BeanConfig {
+
+    @Bean(name = "defaultWatcher")
+    public Watcher getDefaultWatcher(){
+        return new MyWatcher();
+    }
+}*/
---
+++ zookeeper/demo/src/main/java/cn/com/yui/study/zookeeper/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package cn.com.yui.study.zookeeper.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XuZhuohao
+ * @date 2019/3/27
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
